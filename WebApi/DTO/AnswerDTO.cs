7bd34078e2dccabb3a501f2168d60644ef548299
michal.faber <kiryometsy@gmail.com>
2024-03-20 15:01:39 +0100

    lab 3

---
+++ WebApi/DTO/AnswerDTO.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApi.DTO;
+
+public class AnswerDTO
+{
+    public string Answer { get; set; }
+}
---
+++ WebApi/DTO/QuizItemDTO.cs
@@ -0,0 +1,26 @@
+﻿using BackendLab01;
+
+namespace WebApi.DTO;
+
+public class QuizItemDTO
+{
+    private static Random rand = new Random();
+    public int Id { get; set; }
+    public string Question { get; set; }
+    public List<string> Options { get; set; }
+    public static QuizItemDTO Of(QuizItem item)
+    {
+        var options = new List<string>(item.IncorrectAnswers)
+        {
+            item.CorrectAnswer
+        };
+        Random random = new Random();
+        options.Sort((a,b) => 1 - random.Next(3));
+        return new QuizItemDTO()
+        {
+            Id = item.Id,
+            Question = item.Question,
+            Options = options
+        };
+    }
+}
